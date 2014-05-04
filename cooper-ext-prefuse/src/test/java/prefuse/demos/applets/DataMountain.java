d3cc9f3bcbcd021f723196b9682063dbf17a8cbb
jdepend <wangdg@neusoft.com>
2014-05-04 21:57:24 +0800

    init

---
+++ cooper-ext-prefuse/src/test/java/prefuse/demos/applets/DataMountain.java
@@ -0,0 +1,11 @@
+package prefuse.demos.applets;
+
+import prefuse.util.ui.JPrefuseApplet;
+
+public class DataMountain extends JPrefuseApplet {
+
+	public void init() {
+		this.setContentPane(prefuse.demos.DataMountain.demo());
+	}
+
+} // end of class DataMountain
---
+++ cooper-ext-prefuse/src/test/java/prefuse/demos/applets/TreeMap.java
@@ -0,0 +1,11 @@
+package prefuse.demos.applets;
+
+import prefuse.util.ui.JPrefuseApplet;
+
+public class TreeMap extends JPrefuseApplet {
+
+	public void init() {
+		this.setContentPane(prefuse.demos.TreeMap.demo("/chi-ontology.xml.gz", "name"));
+	}
+
+} // end of class TreeMap
