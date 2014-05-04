35e5666ce6ca4370852755cac434cfe51371c9a8
jdepend <wangdg@neusoft.com>
2014-05-04 21:07:48 +0800

    init

---
+++ db/doc/dbscript.sql
@@ -0,0 +1,157 @@
+--common
+
+drop table analysissummry;
+
+create table analysissummry (
+id                   varchar(36)                    not null,
+client               varchar(20),
+username             varchar(32),
+groupname            varchar(32),
+commandname          varchar(64),
+LC                   integer,
+CN                   integer,
+CC                   integer,
+AC                   integer,
+Ca                   integer,
+Ce                   integer,
+A                    float,
+I                    float,
+D                    float,
+Coupling             float,
+Cohesion             float,
+Balance              float,
+Encapsulation        float,
+OO                   float,
+UC                   int,
+createdate           timestamp                       default now(),
+primary key (id)
+);
+
+
+drop table analysisdata;
+
+create table analysisdata (
+id                   varchar(36)                    not null,
+result               blob,
+primary key (id)
+);
+
+
+
+--client
+
+drop table score;
+
+create table score (
+id                   varchar(36)                    not null,
+groupname            varchar(32),
+commandname          varchar(64),
+LC                   integer,
+Score                Float,
+D                    float,
+Balance              float,
+Relation             float,
+Encapsulation        float,
+OO                   float,
+createdate           timestamp                       default now(),
+primary key (id)
+);
+
+
+drop table scoredata;
+
+create table scoredata (
+id                   varchar(36)                    not null,
+result               blob,
+primary key (id)
+);
+
+drop table scoreext;
+
+create table scoreext (
+id                   VARCHAR(36)                    not null,
+scoreid              VARCHAR(36),
+itemname             VARCHAR(32),
+score                float,
+primary key (id)
+);
+
+drop table busilog;
+
+create table busilog (
+id                   varchar(36)                    not null,
+username             varchar(32),
+operation            varchar(64),
+createdate           timestamp                       default now(),
+primary key (id)
+);
+
+
+--server
+
+drop table analyzer;
+
+drop table scorelist;
+
+drop table user;
+
+drop table useraction;
+
+create table analyzer (
+classname            varchar(256),
+name                 varchar(256)                   not null,
+tip                  varchar(256),
+bigtip               varchar(1024),
+type                 varchar(32),
+defaultdata          blob,
+def                  blob,
+username             varchar(256),
+createdate           timestamp,
+uploaddate           timestamp                       default now(),
+primary key (name)
+);
+
+create table scorelist (
+id                   varchar(36)                    not null,
+ip                   varchar(32),
+username             varchar(32),
+groupname            varchar(32),
+commandname          varchar(64),
+LC                   integer,
+Score                Float,
+D                    float,
+Balance              float,
+Relation             float,
+Encapsulation        float,
+createdate           timestamp,
+uploaddate           timestamp                       default now(),
+primary key (id)
+);
+
+create table user (
+name                 varchar(36)                    not null,
+password             varchar(32),
+dept                 varchar(32),
+integral             int,
+valid                char(1),
+primary key (name)
+);
+
+create table useraction (
+id                   varchar(36)                    not null,
+username             varchar(32),
+operation            varchar(64),
+ip                   varchar(32),
+createdate           timestamp,
+gartherdate          timestamp                       default now(),
+primary key (id)
+);
+
+
+insert into user values('admin', null, null, null,'Y');
+insert into user values('AnonymousUser', null, null, 0, 'Y');
+insert into user values('test', 'test', null, 1000, 'Y');
+
+
+
+
